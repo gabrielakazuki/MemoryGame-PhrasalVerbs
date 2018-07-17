d3d4f18cbb3a864ee0b94216a74396ca31b90ac8
gabrielakazuki <40675716+gabrielakazuki@users.noreply.github.com>
2018-07-17 20:14:41 -0300

    Update frame.rkt

--- frame.rkt
+++ frame.rkt
@@ -7,10 +7,13 @@
                [width 500]
                [height 500]))
 
-(new button% [parent frame]
-             [label "Iniciar"]
-             ; Callback procedure for a button click:
-             )
+(define row1
+  (new horizontal-panel%
+       [parent frame]
+       [min-width   50]
+       [min-height  10]
+       [style       '(border)]
+       [stretchable-height #f]))
 
 (define c0 (make-object bitmap% "images/green.png"))
 (define c1 (make-object bitmap% "images/red.png"))
@@ -22,6 +25,34 @@
 (define c7 (make-object bitmap% "images/yellow.png"))
 (define c8 (make-object bitmap% "images/gray.png"))
 
+(define msg (new message% [parent row1]
+                 [label "Press Start!"]))
+
+(define begin 0.0)
+(define diff 0.0)
+
+; Timer
+(define timer (new timer%
+                   [notify-callback (lambda ()
+                                      (set! diff (- (current-inexact-milliseconds) begin))
+                                      (send msg set-label (number->string diff)))]
+                   [interval #f]))
+
+; Start button
+(new button% [parent row1] [label "Start"]
+     (callback (lambda (button event)
+                 (set! begin (- (current-inexact-milliseconds) diff))
+                 (send timer start 10))))
+
+; Stop button
+(new button% [parent row1] [label "Stop"]
+     (callback (lambda (button event)
+                 (send timer stop))))
+
+;(define button-icon
+;  (read-bitmap "images/red.png"))
+;(define button
+;  (new button% [label button-icon] [parent frame]))
 
 (define mycanvas%
   (class canvas%
@@ -33,6 +64,11 @@
       (send my-dc draw-bitmap c4 50 176) (send my-dc draw-bitmap c5 160 176) (send my-dc draw-bitmap c6 270 176) (send my-dc draw-bitmap c7 380 176)
       (send my-dc draw-bitmap c0 50 342) (send my-dc draw-bitmap c1 160 342) (send my-dc draw-bitmap c2 270 342) (send my-dc draw-bitmap c3 380 342)
       (send my-dc draw-bitmap c4 50 508) (send my-dc draw-bitmap c5 160 508) (send my-dc draw-bitmap c6 270 508) (send my-dc draw-bitmap c7 380 508)
+
+      (send my-dc draw-bitmap c8 50 10) (send my-dc draw-bitmap c8 160 10) (send my-dc draw-bitmap c8 270 10) (send my-dc draw-bitmap c8 380 10)
+      (send my-dc draw-bitmap c8 50 176) (send my-dc draw-bitmap c8 160 176) (send my-dc draw-bitmap c8 270 176) (send my-dc draw-bitmap c8 380 176)
+      (send my-dc draw-bitmap c8 50 342) (send my-dc draw-bitmap c8 160 342) (send my-dc draw-bitmap c8 270 342) (send my-dc draw-bitmap c8 380 342)
+      (send my-dc draw-bitmap c8 50 508) (send my-dc draw-bitmap c8 160 508) (send my-dc draw-bitmap c8 270 508) (send my-dc draw-bitmap c8 380 508)
         ))))
       
 (define c (new mycanvas% [parent frame]))
