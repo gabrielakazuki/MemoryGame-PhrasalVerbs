f6a599a9a51fc472a210d9dde9693b0f127cd529
gabrielakazuki <40675716+gabrielakazuki@users.noreply.github.com>
2018-07-16 21:48:49 -0300

    Add files via upload

---
+++ frame.rkt
@@ -0,0 +1,35 @@
+#lang racket/gui
+(require net/url)
+
+
+(define frame (new frame% 
+               [label "Test"]
+               [width 500]
+               [height 500]))
+
+(new button% [parent frame]
+             [label "Iniciar"]
+             ; Callback procedure for a button click:
+             )
+
+(define c0 (make-object bitmap% (get-pure-port 
+                                 (string->url "file:///C:/Users/Gabriela/Desktop/GABRIELA/jogo/sit.png"))))
+(define c1 (make-object bitmap% (get-pure-port 
+                                 (string->url "file:///C:/Users/Gabriela/Desktop/GABRIELA/jogo/sleep.png"))))
+(define c2 (make-object bitmap% (get-pure-port 
+                                 (string->url "file:///C:/Users/Gabriela/Desktop/GABRIELA/jogo/sleep.png"))))
+(define c3 (make-object bitmap% (get-pure-port 
+                                 (string->url "file:///C:/Users/Gabriela/Desktop/GABRIELA/jogo/sit.png"))))
+
+(define mycanvas%
+  (class canvas%
+    (super-new)
+    (inherit get-dc)
+    (define/override (on-paint)
+      (let ([my-dc (get-dc)])
+        (send my-dc draw-bitmap c0 150 50) (send my-dc draw-bitmap c1 225 50) (send my-dc draw-bitmap c2 150 150) (send my-dc draw-bitmap c3 225 150)))))
+
+(define c (new mycanvas% [parent frame]))
+
+(send frame show #t)
+
